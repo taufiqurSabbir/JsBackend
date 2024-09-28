fb909d61ceb804c23ef80c185d7ec8af37661173
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2024-09-28 10:46:10 +0600

    api added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send('Hello World!');
 });
 
+app.get('/getNationality', async (req: Request, res: Response) => {
+   
+        const BASE_URL = 'https://api.nationalize.io';
+        const response = await axios.get(`${BASE_URL}/?name=${req.query['name']}`);
+        
+        console.log(response.data);
+        res.status(200).send(response.data);
+});
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
